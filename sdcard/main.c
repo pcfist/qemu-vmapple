058dcff71c5d06a14c638af3c6606a739c9cb446
Alexander Graf <agraf@suse.de>
2018-07-23 11:55:49 +0200

    XXX
    Signed-off-by: Alexander Graf <agraf@suse.de>

--- sdcard/main.c
+++ sdcard/main.c
@@ -195,6 +195,35 @@ static int sdcard_init(char *filename)
     return 0;
 }
 
+/* XXX remove */
+static void init_perfcounters (int32_t do_reset, int32_t enable_divider) {
+#ifdef __arm__
+  // in general enable all counters (including cycle counter)
+  int32_t value = 1;
+
+  // peform reset:
+  if (do_reset) {
+      value |= 2;     // reset all counters to zero.
+      value |= 4;     // reset cycle counter to zero.
+    }
+
+  if (enable_divider)
+    value |= 8;     // enable "by 64" divider for CCNT.
+
+  value |= 16;
+
+  // program the performance-counter control-register:
+  asm volatile ("MCR p15, 0, %0, c9, c12, 0\t\n" :: "r"(value));
+
+  // enable all counters:
+  asm volatile ("MCR p15, 0, %0, c9, c12, 1\t\n" :: "r"(0x8000000f));
+
+  // clear overflows:
+  asm volatile ("MCR p15, 0, %0, c9, c12, 3\t\n" :: "r"(0x8000000f));
+#endif
+}
+
+
 int main(int argc, char **argv)
 {
     Error *err = NULL;
@@ -248,6 +277,7 @@ int main(int argc, char **argv)
         return 1;
     }
 
+    init_perfcounters(1, 1);
     last_time = cpu_get_host_ticks();
     usleep(10000);
     time_per_us = (cpu_get_host_ticks() - last_time) / 10000;
